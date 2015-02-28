790d7e396bd1c88a38d37554671d7aacbe986be6
mizokami <suzunatsu@yahoo.com>
2015-02-28 16:42:28 +0900

    Init

---
+++ lib/itunes_connect/autoingestion.rb
@@ -0,0 +1,7 @@
+require "itunes_connect/autoingestion/version"
+
+module ItunesConnect
+  module Autoingestion
+    # Your code goes here...
+  end
+end
---
+++ lib/itunes_connect/autoingestion/version.rb
@@ -0,0 +1,5 @@
+module ItunesConnect
+  module Autoingestion
+    VERSION = "0.0.1"
+  end
+end
